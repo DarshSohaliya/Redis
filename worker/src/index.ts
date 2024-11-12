beb30de18e88c916a16b69272232894f1dea8d90
Darsh Sohaliya <darshsohaliya029290@gmail.com>
2024-11-12 14:57:12 +0530

    Redis

---
+++ worker/src/index.ts
@@ -0,0 +1,19 @@
+import { createClient } from "redis";
+
+const client = createClient()
+
+async function main() {
+await client.connect()
+
+    while(true){
+        const response = await client.brPop("submission",0);
+          console.log(response);
+      
+          await new Promise((resolve) => setTimeout(resolve,1000))
+      
+          console.log("Processed user submission");
+          
+          
+      }
+}
+main()
